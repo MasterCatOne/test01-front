b2f12acb3df46c1529061a9caee1050161e63aa3
Gakki <2251000810@qq.com>
2024-12-06 18:04:27 +0800

    fear:文档编辑

--- src/api/article.js
+++ src/api/article.js
@@ -1,9 +1,18 @@
-import {get} from "@/utils/request.js";
-// 获取用户信息
+import {get,put} from "@/utils/request.js";
+
+// 获取文章信息
 export const articleListApi = (data) => {
     return get({
         // 请求的地址
         url: "/api/article/getArticleList",
+        data,
+    });
+};
+// 修改文章
+export const userUpdateApi = (data) => {
+    return put({
+        // 请求的地址
+        url: "/api/article/updateArticle",
         data,
     });
 };
--- src/views/article/ArticleList.vue
+++ src/views/article/ArticleList.vue
@@ -1,8 +1,9 @@
 <script setup>
 /*-----------------------------------------搜索文章列表---------------------------------------*/
 // 文章列表数据
-import {onMounted, ref} from "vue";
-import {articleListApi} from "../../api/article";
+import {onMounted, reactive, ref} from "vue";
+import {articleListApi, userUpdateApi} from "../../api/article";
+import {ElMessage} from "element-plus";
 
 const articles = ref([
   {id: 1, title: '文章标题1', content: '文章内容1', createdTime: '2024-02-20', author: 'John Doe'},
@@ -19,6 +20,35 @@ onMounted(() => {
   // 在这里执行初始化操作，例如从服务器获取数据
   searchList();
 });
+/*-----------------------------------------编辑文章---------------------------------------*/
+const editDialogVisible = ref(false);// 控制编辑对话框的显示/隐藏
+// 编辑文章的数据对象
+const editArticleData = reactive({
+  id: '',
+  title: '',
+  content: '',
+  author: ''
+});
+// 显示编辑对话框，传入要编辑的文章对象
+const editArticle = (article) => {
+  editDialogVisible.value = true;// 显示弹窗
+  editArticleData.id = article.id;// 文章ID
+  editArticleData.title = article.title;// 文章标题
+  editArticleData.content = article.content;// 文章内容
+  editArticleData.author = article.author;// 作者
+};
+//提交编辑
+const submitEdit = async () => {
+  editDialogVisible.value = false;//隐藏弹窗
+  const res = await userUpdateApi(editArticleData);//调用编辑api
+  if (res.code === 0) {
+    ElMessage({
+      type: 'success',
+      message: '文章编辑成功'
+    });
+    await searchList();// 刷新列表数据
+  }
+};
 </script>
 <template>
   <!-- ----------------------------------------面包屑---------------------------------------------- -->
@@ -35,11 +65,30 @@ onMounted(() => {
     <el-table-column prop="author" lable="作者"></el-table-column>
     <el-table-column lable="操作">
       <template #default="scope">
-        <el-button type="primary" size="mini" @click="handleEdit(scope.row)">编辑</el-button>
+        <el-button type="primary" size="mini" @click="editArticle(scope.row)">编辑</el-button>
         <el-button type="danger" size="mini" @click="handleDelete(scope.row)">删除</el-button>
       </template>
     </el-table-column>
   </el-table>
+  <!-- ----------------------------------------编辑文章区域----------------------------------------- -->
+  <el-dialog v-model="editDialogVisible" title="编辑文章" width="500">
+    <el-form label-width="80px">
+      <el-form-item label="文章标题">
+        <el-input v-model="editArticleData.title"></el-input>
+      </el-form-item>
+      <el-form-item label="文章内容">
+        <el-input type="textarea" :rows="6" placeholder="请输入文章内容" v-model="editArticleData.content"></el-input>
+      </el-form-item>
+      <el-form-item label="作者">
+        <el-input v-model="editArticleData.author"></el-input>
+      </el-form-item>
+    </el-form>
+    <span class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="submitEdit">确定</el-button>
+      </span>
+  </el-dialog>
 </template>
+
 <style scoped>
 </style>
